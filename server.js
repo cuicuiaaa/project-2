397b1e3fe801b4fb09ba107d7a3db4d7fd4a7fb9
prithvipsingh <ppsdeora@gmail.com>
2020-05-26 19:43:36 +1000

    updates

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
+const socket = require('socket.io')
+const Sequelize = require('sequelize')
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 
